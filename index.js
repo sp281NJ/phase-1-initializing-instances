9f350d2e48c08dcbfe433bbe40b26f5a14483ce9
Stephen <pena731.junk@gmail.com>
2023-11-21 19:11:39 -0500

    initializing-instance

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your code here
+///  samples
+/*
+class Dog{
+constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+}
+
+let bigFluffyDog1 = new Dog("Buzz", "greatPyrenees");
+let bigFluffyDog2 = new Dog("Woody", "labrador");
+*/
+
+
+class Breakfast{
+    constructor(food,drink){
+        this.food = food;
+        this.drink = drink;
+    }
+
+}
+
+
+
+class Lunch{
+    constructor(salad,soup,drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+
+class Dinner{
+    #dessert;
+    constructor(salad,soup,entree,dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.#dessert = dessert;
+
+    }
+
+}
+
